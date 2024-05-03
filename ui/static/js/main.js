caeb7fcc4e56872e427e41a3389ac8d48e768ce1
pwilliams-ck <pwilliams@cloudkey.io>
2024-05-03 00:38:24 -0500

    add slog request middleware

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,9 +1,8 @@
-let navLinks = document.querySelectorAll("nav a");
-for (let i = 0; i < navLinks.length; i++) {
-  let link = navLinks[i];
+var navLinks = document.querySelectorAll("nav a");
+for (var i = 0; i < navLinks.length; i++) {
+  var link = navLinks[i];
   if (link.getAttribute("href") == window.location.pathname) {
     link.classList.add("live");
     break;
   }
 }
-
